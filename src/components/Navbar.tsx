736dd2dfc8f5f45e69463a3a8e05a6ae88baeebe
Noah Coleman <noahjcoleman25@gmail.com>
2025-08-04 14:58:40 -0600

    Hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar: React.FC = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsNavOpen(!isNavOpen);
+    };
+
+    const closeMenu = () => {
+        setIsNavOpen(false);
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg bg-light fixed-top shadow-sm">
-            <div className="container d-flex flex-wrap justify-content-between align-items-center">
+        <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top shadow-sm">
+            <div className="container">
                 {/* Brand */}
-                <NavLink to="/" className="navbar-brand fw-bold fs-4" style={{ color: "#dd783f" }}>
+                <NavLink
+                    to="/"
+                    className="navbar-brand fw-bold fs-4"
+                    style={{ color: "#dd783f" }}
+                    onClick={closeMenu}
+                >
                     Garrett Nichols
                 </NavLink>
 
+                {/* Hamburger toggle button */}
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    onClick={handleToggle}
+                    aria-controls="navbarNav"
+                    aria-expanded={isNavOpen}
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-                {/* Navigation Links */}
-                <ul className="nav">
-                    <li className="nav-item">
-                        <NavLink to="/" end className="nav-link text-dark">
-                            Stories
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/gallery" className="nav-link text-dark">
-                            Gallery
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/memorial-video" className="nav-link text-dark">
-                            Memorial Video
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/message" className="nav-link text-dark">
-                            Message the Family
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/jokes" className="nav-link text-dark">
-                            Jokes & Pick-Up Lines
-                        </NavLink>
-                    </li>
-                </ul>
-
+                {/* Collapsible menu */}
+                <div
+                    className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`}
+                    id="navbarNav"
+                >
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <NavLink to="/" end className="nav-link text-dark" onClick={closeMenu}>
+                                Stories
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/gallery" className="nav-link text-dark" onClick={closeMenu}>
+                                Gallery
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/memorial-video" className="nav-link text-dark" onClick={closeMenu}>
+                                Memorial Video
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/message" className="nav-link text-dark" onClick={closeMenu}>
+                                Message the Family
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/jokes" className="nav-link text-dark" onClick={closeMenu}>
+                                Jokes & Pick-Up Lines
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );

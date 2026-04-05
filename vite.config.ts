7b11ce3d64a94323a3d196baa9e52b3a335a706f
Noah <noahjcoleman@gmail.com>
2026-04-04 23:42:05 -0600

    Fix custom domain asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/garrett-website/',
+  base: '/',
   plugins: [react()],
   build: { outDir: 'dist' } // default, just being explicit
 })
